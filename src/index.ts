7b5bebbe0439a263a681995819664f2f14c7cdfb
phuongtung1 <phuongtung1231992@yahoo.com>
2021-10-11 14:11:58 +0800

    update

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import * as Modules from './core/modules/index';
+import * as InlineData from './core/inline/inline';
+import * as _parameterTypes from './core/_parameterTypes';
+
+export {Modules, InlineData, _parameterTypes};
