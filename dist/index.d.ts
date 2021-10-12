738e429bc4a7075da8ae1a5a41bce7db2346cd9e
phuongtung1 <phuongtung1231992@yahoo.com>
2021-10-12 14:35:37 +0800

    library update

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,4 +1,11 @@
 import * as Modules from './core/modules/index';
-import * as InlineData from './core/inline/inline';
-import * as _parameterTypes from './core/_parameterTypes';
-export { Modules, InlineData, _parameterTypes };
+import { inline_query_expr, inline_sort_expr, inline_func } from './core/inline/inline';
+import { _parameterTypes, _varString } from './core/_parameterTypes';
+import { _EEntType, _EFilterOperator } from './core/modules/basic/query';
+import { EEntType, EEntTypeStr, EAttribNames, LONGLAT } from './libs/geo-info/common';
+import { GIModel } from './libs/geo-info/GIModel';
+import { GIMetaData } from './libs/geo-info/GIMetaData';
+import { GIAttribsThreejs } from './libs/geo-info/attribs/GIAttribsThreejs';
+import { xfromSourceTargetMatrix } from './libs/geom/matrix';
+import { sortByKey } from './libs/util/maps';
+export { Modules, _EEntType, _EFilterOperator, EEntType, EEntTypeStr, EAttribNames, GIModel, GIMetaData, GIAttribsThreejs, xfromSourceTargetMatrix, sortByKey, LONGLAT, _parameterTypes, _varString, inline_query_expr, inline_sort_expr, inline_func, };
--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,1 @@
-import * as Modules from './dist/core/modules/index';
-import * as InlineData from './dist/core/inline/inline';
-import * as _parameterTypes from './dist/core/_parameterTypes';
-export { Modules, InlineData, _parameterTypes };
+export * from './dist';
